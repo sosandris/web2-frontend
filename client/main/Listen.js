be38ebfdc6ccb1153c6e4e01f7b8adcb64e3a826
szabidu <szabi@tilos.hu>
2021-03-18 09:37:01 +0100

    kevesebb listen panel a jobb oldalon

--- client/main/Listen.js
+++ client/main/Listen.js
@@ -9,7 +9,8 @@ angularModule.component('listenNow', {
                <a href="http://stream.tilos.hu:8000/tilos" class="btn btn-lg col-xs-12 btn-danger">Élő adás (256)</a>\
                <a href="/podcast" class="btn btn-lg col-xs-12 rss btn-danger">Podcast <span class="icon-rss-alt"></a>\
                <a href="/archive" class="btn btn-lg col-xs-12 btn-info">Archívum</a -->\
-               <p>További streamek:</p>\
+               <div class="panel-heading">További streamek:</div>
+               <div class="panel-body">
                <a href="http://stream.tilos.hu/jazzisdead" class="btn btn-lg col-xs-12 btn-danger"> jazz is dead? </a>\
                <a href="http://stream.tilos.hu/szoveg" class="btn btn-lg col-xs-12 btn-danger"> Okosodj! </a>\
                <a href="http://stream.tilos.hu/mese" class="btn btn-lg col-xs-12 btn-danger">Tilos Mesék </a>\
