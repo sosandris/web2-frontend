7d78d5a0b2bfeb519a25fc8ff40d3dab9a25a556
szabidu <szabi@tilos.hu>
2021-03-18 09:30:48 +0100

    Kicsi hallgass panel

--- client/main/Listen.js
+++ client/main/Listen.js
@@ -3,8 +3,8 @@ var angularModule = require("main/Main");
 require("main/Listen.css");
 
 angularModule.component('listenNow', {
-    template: '<!-- div class="panel panel-default listen-panel">\
-               <div class="panel-heading">Hallgass</div>\
+    template: '<div class="panel panel-default listen-panel">\
+               <!-- div class="panel-heading">Hallgass</div>\
                <div class="panel-body">\
                <a href="http://stream.tilos.hu:8000/tilos" class="btn btn-lg col-xs-12 btn-danger">Élő adás (256)</a>\
                <a href="/podcast" class="btn btn-lg col-xs-12 rss btn-danger">Podcast <span class="icon-rss-alt"></a>\
