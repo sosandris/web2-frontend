392ef412b430a0147f20a6930353097c68f1cec4
szabidu <szabi@tilos.hu>
2021-03-18 08:56:00 +0100

    kevesebb listen a jobb oldalon

--- client/main/Listen.js
+++ client/main/Listen.js
@@ -3,12 +3,12 @@ var angularModule = require("main/Main");
 require("main/Listen.css");
 
 angularModule.component('listenNow', {
-    template: '<div class="panel panel-default listen-panel">\
+    template: '<!-- div class="panel panel-default listen-panel">\
                <div class="panel-heading">Hallgass</div>\
                <div class="panel-body">\
                <a href="http://stream.tilos.hu:8000/tilos" class="btn btn-lg col-xs-12 btn-danger">Élő adás (256)</a>\
                <a href="/podcast" class="btn btn-lg col-xs-12 rss btn-danger">Podcast <span class="icon-rss-alt"></a>\
-               <a href="/archive" class="btn btn-lg col-xs-12 btn-info">Archívum</a>\
+               <a href="/archive" class="btn btn-lg col-xs-12 btn-info">Archívum</a -->\
                <p>További streamek:</p>\
                <a href="http://stream.tilos.hu/jazzisdead" class="btn btn-lg col-xs-12 btn-danger"> jazz is dead? </a>\
                <a href="http://stream.tilos.hu/szoveg" class="btn btn-lg col-xs-12 btn-danger"> Okosodj! </a>\
