10e8ef9f9a6e0dd8e23e9bc66189692b086c95a9
szabidu <szabi@tilos.hu>
2021-04-06 12:14:00 +0200

    élő adás panel

--- client/main/Listen.js
+++ client/main/Listen.js
@@ -4,10 +4,10 @@ require("main/Listen.css");
 
 angularModule.component('listenNow', {
     template: '<div class="panel panel-default listen-panel">\
-               <!-- div class="panel-heading">Hallgass</div>\
+               <div class="panel-heading">Hallgass</div>\
                <div class="panel-body">\
                <a href="http://stream.tilos.hu:8000/tilos" class="btn btn-lg col-xs-12 btn-danger">Élő adás (256)</a>\
-               <a href="/podcast" class="btn btn-lg col-xs-12 rss btn-danger">Podcast <span class="icon-rss-alt"></a>\
+               <!-- a href="/podcast" class="btn btn-lg col-xs-12 rss btn-danger">Podcast <span class="icon-rss-alt"></a>\
                <a href="/archive" class="btn btn-lg col-xs-12 btn-info">Archívum</a -->\
                <div class="panel-heading">További streamek:</div>\
                <div class="panel-body">\
